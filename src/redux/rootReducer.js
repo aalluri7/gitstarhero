7dab17b7a7942d57fe8e29909e51dac6fc843645
adi <findalluri@bitbucket.org>
2019-02-05 11:29:59 -0800

    publish

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import todoReducer from './Todo/reducer';
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-  todos: todoReducer
-});
-
-export default rootReducer;
