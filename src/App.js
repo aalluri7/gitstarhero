c5e80b081929adf874a4de164092053d25c2d7db
adi <findalluri@bitbucket.org>
2019-02-06 09:46:30 -0800

    Material-UI boiler plate and appbar

--- src/App.js
+++ src/App.js
@@ -4,27 +4,14 @@ import './App.css';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 import AddTodo from './containers/AddTodo';
+import Main from './containers/main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Provider store={store}>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reawefload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            <AddTodo />
-          </header>
+          <Main />
         </Provider>
       </div>
     );
