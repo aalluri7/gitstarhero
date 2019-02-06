c1a11dba7035e9dfac7fc67f20cef996d922bdc3
adi <findalluri@bitbucket.org>
2019-02-06 10:33:09 -0800

    Adding a GAE python server to exchange the code for an auth token

---
+++ GAE/main.py
@@ -0,0 +1,29 @@
+from flask import Flask,request, redirect
+import requests
+from secrets import client_id, client_secret
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+def generateToken(code):
+    url = 'https://github.com/login/oauth/access_token?client_id='+client_id+'&client_secret='+client_secret+'&code='+code
+    return requests.post(url).text
+
+@app.route('/gitstarhero/callback')
+def callback():
+    code = request.args.get('code')
+    token = generateToken(code)
+    return redirect('https://aalluri7.github.io/gitstarhero/?'+token)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
